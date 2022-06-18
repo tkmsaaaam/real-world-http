d59733783b148bcc571b4dd1e41338ae989ffa5c
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-06-18 11:49:14 +0900

    chapter10.6.1 P318

---
+++ chapter10/github.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+)
+
+var clientID = ""
+var clientSecret = ""
+var redirectURL = "http://localhost:18888"
+var state = ""
+
+func main() {
+	conf := &oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Scopes:       []string{"user:email", "gist"},
+		Endpoint:     github.Endpoint,
+	}
+	var token *oauth2.Token
+	file, err := os.Open("access_token.json")
+	if os.IsNotExist(err) {
+		url := conf.AuthCodeURL(state, oauth2.AccessTypeOnline)
+		code := make(chan string)
+		var server *http.Server
+		server = &http.Server{
+			Addr: ":18888",
+			Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "text/html")
+				io.WriteString(w, "<html><script>window.open('about:blank','_self').close()</script></html>")
+				w.(http.Flusher).Flush()
+				code <- r.URL.Query().Get("code")
+				server.Shutdown(context.Background())
+			}),
+		}
+		go server.ListenAndServe()
+		open.Start(url)
+		token, err = conf.Exchange(oauth2.NoContext, <-code)
+		if err != nil {
+			panic(err)
+		}
+		file, err := os.Create("access_token.json")
+		if err != nil {
+			panic(err)
+		}
+		json.NewEncoder(file).Encode(token)
+	} else if err == nil {
+		token = &oauth2.Token{}
+		json.NewDecoder(file).Decode(token)
+	} else {
+		panic(err)
+	}
+	client := oauth2.NewClient(oauth2.NoContext, conf.TokenSource(oauth2.NoContext, token))
+}
